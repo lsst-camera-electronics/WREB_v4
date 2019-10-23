8f619710f42256ce48bde72fa67b3ead0023239f
Stefano Russo <srusso@slac.stanford.edu>
2019-10-23 13:41:22 -0700

    new SCI with new GTX parameters and look-at-me mask defoult state changed

--- firmware/targets/WREB_v4/Version.vhd
+++ firmware/targets/WREB_v4/Version.vhd
@@ -23,9 +23,9 @@ package Version is
 -------------------------------------------------------------------------------
   -- 102c4000  Compiling WREB_v4 for DAQ v1
 
-  constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"11374004";  -- MAKE_VERSION
+  constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"11384004";  -- MAKE_VERSION
 
-  constant BUILD_STAMP_C : string := "WREB_v4: Vivado v2015.3 (x86_64) Built Fri Mar  1 11:03:46 CET 2019 by srusso";
+  constant BUILD_STAMP_C : string := "WREB_v4: Vivado v2015.3 (x86_64) Built Wed Oct 23 12:29:02 PDT 2019 by srusso";
 
 end Version;
 
